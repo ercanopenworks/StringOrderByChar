27f9c6a08f0157ded51e74b8419de7967dfa393d
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-11-15 05:51:37 +0300

    was completed

---
+++ StringChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringChallenge
+{
+    class Program
+    {
+        public static string StringChallenge(string str)
+        {
+
+            // code goes here  
+            List<char> temp = new List<char>();
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i]))
+                {
+                    temp.Add(str[i]);
+                    
+                }
+
+            }
+            temp.Sort();
+
+            return string.Join("", temp);
+
+        }
+
+        static void Main()
+        {
+            // keep this function call here
+            Console.WriteLine(StringChallenge(Console.ReadLine()));
+        }
+    }
+}
